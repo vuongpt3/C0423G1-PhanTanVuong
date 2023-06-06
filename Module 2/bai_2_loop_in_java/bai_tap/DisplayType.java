b4e3227327904ab198eacff2e70555510ab2eb1a
vuongpt3 <thailun.1995@gmai.com>
2023-06-06 09:52:34 +0700

    nộp bài tập

--- Module 2/bai_2_loop_in_java/bai_tap/DisplayType.java
+++ Module 2/bai_2_loop_in_java/bai_tap/DisplayType.java
@@ -9,15 +9,37 @@ public class DisplayType {
         int select = input.nextInt();
         switch (select) {
             case 1:
-                System.out.println("Enter width");
-                int length = input.nextInt();
                 System.out.println("Enter length");
+                int length = input.nextInt();
+                System.out.println("Enter width");
                 int width = input.nextInt();
-                    for (int i = 0; i < length; i++) {
-                        for (int j = 0; j < width; j++) {
-                            System.out.println("*");
+                    for (int i = 0; i <= width; i++) {
+                        for (int j = 0; j <= length; j++) {
+                            System.out.print("* ");
                         }
+                        System.out.println(" ");
                     }
+                break;
+
+            case 2 :
+                System.out.println("Enter hight");
+                int hight = input.nextInt();
+                for (int i = 0; i <= hight; i++) {
+                    for (int j = 0; j <= i; j++) {
+                            System.out.print("* ");
+                    }
+                    System.out.println(" ");
+                }
+                break;
+            case 3 :
+                System.out.println("Enter hight");
+                int hight1 = input.nextInt();
+                for (int i = hight1; i >= 0; i--) {
+                    for (int j = 0; j <= i; j++) {
+                            System.out.print("* ");
+                    }
+                    System.out.println(" ");
+                }
                 break;
             default:
                 System.out.println("Are you Oke");
